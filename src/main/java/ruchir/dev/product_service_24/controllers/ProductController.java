6c5edfe2d9848bd871b3b777598114a641e7f6f6
Ruchir Mandora <ruchirm702@gmail.com>
2024-07-03 22:40:02 +0530

    Added API

--- src/main/java/ruchir/dev/product_service_24/controllers/ProductController.java
+++ src/main/java/ruchir/dev/product_service_24/controllers/ProductController.java
@@ -16,7 +16,6 @@ import java.util.List;
 public class ProductController {
 
     private ProductService productService;
-
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
